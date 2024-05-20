5200e560c3974f48ffe581150869f32f60b2f702
Ed Cavalcante <131734732+edmozer@users.noreply.github.com>
2024-05-19 19:43:39 -0600

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -21,14 +21,14 @@ describe('Login', () => {
     })
 
     it('senha incorreta', () => {
-        cy.login('qa', 'teste')
-        cy.get(toast)
-            .should('have.text', 'Oops! Credenciais inválidas :(')
+      cy.login('qa', 'teste');
+      cy.get(toast)
+          .should('have.text', 'Oops! Credenciais inválidas :(');
     })
 
     it('com sucesso', () => {
-        cy.login('qa', 'xperience')
-        cy.get(modalContent)
-            .should('have.text', 'Suas Credenciais são válidas :)')
+      cy.login('qa', 'xperience');
+      cy.get(modalContent)
+          .should('have.text', 'Suas credenciais são válidas :)');
     })
 })
